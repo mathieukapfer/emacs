3ad55ef770d7b9e890814b16483d74e914b8e269
Mathieu Kapfer <mathieu.kapfer@non.schneider-electric.com>
2017-11-09 14:17:45 +0100

    [grpe] fix colorize hook fct

--- init.el
+++ init.el
@@ -84,7 +84,9 @@
 (require 'ansi-color)
 (defun colorize-compilation-buffer ()
   (let ((inhibit-read-only t))
-    (ansi-color-apply-on-region (point-min) (point-max))))
+    ;;(ansi-color-apply-on-region (point-min) (point-max))))
+    (ansi-color-apply-on-region compilation-filter-start (point))))
+
 (add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
 
 ;; Compilation error
